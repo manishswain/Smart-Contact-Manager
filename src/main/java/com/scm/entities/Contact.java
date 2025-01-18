1811a5f8fe2968b4e24f9d63fdfd63548b930bf4
Manish Swain <manishswain@users.noreply.github.com>
2025-01-18 18:32:09 +0530

    Revert "Modal Show Included"
    This reverts commit b7e0b8055f1ecfddc42730b7d9295ca7eb304d97.

--- src/main/java/com/scm/entities/Contact.java
+++ src/main/java/com/scm/entities/Contact.java
@@ -3,8 +3,6 @@ package com.scm.entities;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -50,7 +48,6 @@ public class Contact {
     private String cloudinaryImagePublicId;
 
     @ManyToOne
-    @JsonIgnore
     private User user;
 
     @OneToMany(mappedBy = "contact", cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
